c2abb4020549e36ee8b65089423986917c85f6fd
deco-L <scotchwitchbroom@gmail.com>
2024-03-27 22:33:24 +0900

    V V

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,22 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/03/20 20:11:14 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/03/21 17:12:34 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#ifndef FIXED_HPP
+
+class Fixed {
+private:
+
+public:
+	Fixed( void ) {}
+	~Fixed( void ){}
+
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+};
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,13 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/03/20 20:11:17 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/03/21 17:12:42 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+#include <cstdlib>
+
+int main( void ) {
+	return (EXIT_SUCCESS);
+}
