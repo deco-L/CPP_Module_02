fd0bb4d2024e9925715455a66fe0052d7b075517
deco-L <scotchwitchbroom@gmail.com>
2024-05-29 17:25:46 +0900

    V V

--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/29 16:38:25 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/05/29 17:25:24 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int main(void) {
   const Point b(-3, -3);
   const Point c(3, -3);
 
-  for (float y = 5; y > -6; y-=0.2) {
-    for (float x = -5; x < 6; x+=0.2) {
+  for (float y = 5; y > -6; y-=0.1) {
+    for (float x = -5; x < 6; x+=0.1) {
       Point px(x, y);
       if (bsp(a, b, c, px))
         std::cout << "\e[38;5;1m.\e[0m";
